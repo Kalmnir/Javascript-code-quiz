bc7580e201156a7a39f6012a7488d35d91f7693a
John-Mark Sackett <jmsackett91@gmail.com>
2021-06-26 16:10:56 -0400

    got the timer working!

--- assets/java.js
+++ assets/java.js
@@ -1,11 +1,14 @@
-var timer = document.getElementById("#timer");
-var score = document.getElementById("#score");
-var question = document.getElementById("#question");
-var answer1 = document.getElementById("#answer1");
-var answer2 = document.getElementById("#answer2");
-var answer3 = document.getElementById("#answer3");
-var answer4 = document.getElementById("#answer4");
-var startButton = document.getElementById("#start-game");
+var timer = document.getElementById("timer");
+var score = document.getElementById("score");
+var testTitle = document.getElementById("test-title");
+var testDescription = document.getElementById("test-description");
+var question = document.getElementById("question");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+var startButton = document.getElementById("start-game");
+var quiz = document.getElementById("quiz");
 
 var question1 = {
     name: "Which of the following is NOT a primitive data type used in Javascript?",
@@ -17,22 +20,39 @@ var question1 = {
 
 
 function runTest() {
-
+    testTitle.classList.add("hide");
+    testDescription.classList.add("hide");
+    startButton.classList.add("hide");
+    quiz.classList.remove("hide");
+    question.textContent = question1.name;
+    answer1.textContent = question1.wrong1;
+    answer2.textContent = question1.wrong2;
+    answer3.textContent = question1.right;
+    answer4.textContent = question1.wrong3;
 
 }
 
+
 function runTimer() {
+
     var secondsRemaining = 65;
     var timerInterval = setInterval(function () {
         secondsRemaining--;
-        timer.textContent = secondLeft;
+        timer.textContent = secondsRemaining;
 
         if (secondsRemaining === 0) {
             clearInterval(timerInterval);
+            sendMessage();
         }
+
     }, 1000);
+
 }
 
+function sendMessage() {
+
+}
 startButton.addEventListener("click", function () {
+    runTest();
     runTimer()
 });
